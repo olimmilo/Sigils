3f31f7a193355d5fe7bcd27835a9a54446b30d91
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2018-02-01 10:16:32 -0500

    Update mcw.py

--- MCW/mcw.py
+++ MCW/mcw.py
@@ -1,1 +1,12 @@
-import bpy
+from PIL import Image, ImageDraw
+
+
+width=1000
+height=1000
+
+imtop = Image.new("RGB", (width,height), "white")
+imbot = Image.new("RGB", (width,height), "white")
+immid = Image.new("RGB", (width,height), "white")
+drawtop = ImageDraw.Draw(imtop)
+drawbot = ImageDraw.Draw(imbot)
+drawmid = ImageDraw.Draw(immid)
