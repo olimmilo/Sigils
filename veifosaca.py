7554f9ab6137da6e97ba63cb98dd07e576d2ca19
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-05 11:39:07 -0500

    Update veifosaca.py

--- veifosaca.py
+++ veifosaca.py
@@ -5,11 +5,14 @@ Shift = 40
 Canvas = 1000
 Buffer = 100
 
+def sqrt(NUM):
+    SQRT=NUM**0.5
+
 def Intersect(CENTER, ANGLE, RADIUS):
 	COOR=[.5,.5]
-	a=ANGLE^2
-	b=(-2*CENTER[0])-((ANGLE^2)*CENTER[0])
-	c=((ANGLE^2)*CENTER[0])+(CENTER[0]^2)-(RADIUS^2)
+	a=ANGLE**2
+	b=(-2*CENTER[0])-((ANGLE**2)*CENTER[0])
+	c=((ANGLE**2)*CENTER[0])+(CENTER[0]**2)-(RADIUS**2)
 	ANGLE=ANGLE%360
 	if b != .5:
 		print("wrong b")
@@ -22,14 +25,14 @@ def Intersect(CENTER, ANGLE, RADIUS):
 	elif ANGLE == 270:
 		COOR=[0,.5]
 	elif ANGLE > 0 and ANGLE < 90:
-		COOR[0]=(-b+sqrt((b^2)-(4*a*c)))/(2*a)
+		COOR[0]=(-b+sqrt((b**2)-(4*a*c)))/(2*a)
 		COOR[1]=(ANGLE*(COOR[0]-CENTER[0]))+CENTER[1]
 	elif ANGLE > 90 and ANGLE < 180:
-		COOR[0]=(-b-sqrt((b^2)-(4*a*c)))/(2*a)
+		COOR[0]=(-b-sqrt((b**2)-(4*a*c)))/(2*a)
 	elif ANGLE > 180 and ANGLE < 270:
-		COOR[0]=(-b-sqrt((b^2)-(4*a*c)))/(2*a)
+		COOR[0]=(-b-sqrt((b**2)-(4*a*c)))/(2*a)
 	elif ANGLE > 270 and ANGLE < 0:
-		COOR[0]=(-b+sqrt((b^2)-(4*a*c)))/(2*a)
+		COOR[0]=(-b+sqrt((b**2)-(4*a*c)))/(2*a)
 	return(COOR)
 
 def DrawSymbol(ROT, SHIFT, CANVAS, BUFFER):
