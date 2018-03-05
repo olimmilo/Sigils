db075c92e79552c97c4a34cf1de8a5ad720f31ac
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-05 08:51:45 -0500

    Update veifosaca.py

--- veifosaca.py
+++ veifosaca.py
@@ -4,3 +4,9 @@ ROT = 0
 SHIFT = 40
 CANVAS = 1000
 BUFFER = 100
+
+def DrawSymbol(R, S, C, B):
+  return()
+
+symbol = Image.new("RGB", (CANVAS,CANVAS), "white")
+drawsymbol = ImageDraw.Draw(symbol)
