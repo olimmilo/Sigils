c007a869b31b8db5fc0e38dc74f0db3cd18be811
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-05 10:47:27 -0500

    Update veifosaca.py

--- veifosaca.py
+++ veifosaca.py
@@ -1,12 +1,44 @@
 from PIL import Image, ImageDraw
 
-ROT = 0
-SHIFT = 40
-CANVAS = 1000
-BUFFER = 100
+Rot = 0
+Shift = 40
+Canvas = 1000
+Buffer = 100
 
-def DrawSymbol(R, S, C, B):
-  return()
+def Intersect(CENTER, ANGLE, RADIUS):
+	COOR=[.5,.5]
+	a=ANGLE^2
+	b=(-2*CENTER[0])-((ANGLE^2)*CENTER[0])
+	c=((ANGLE^2)*CENTER[0])+(CENTER[0]^2)-(RADIUS^2)
+	ANGLE=ANGLE%360
+	if b != .5:
+		print("wrong b")
+	if ANGLE == 0:
+		COOR=[.5,1]
+	elif ANGLE == 90:
+		COOR=[1,.5]
+	elif ANGLE == 180:
+		COOR=[.5,0]
+	elif ANGLE == 270:
+		COOR=[0,.5]
+	elif ANGLE > 0 and ANGLE < 90:
+		COOR[0]=(-b+sqrt((b^2)-(4*a*c)))/(2*a)
+		COOR[1]=(ANGLE*(COOR[0]-CENTER[0]))+CENTER[1]
+	elif ANGLE > 90 and ANGLE < 180:
+		COOR[0]=(-b-sqrt((b^2)-(4*a*c)))/(2*a)
+	elif ANGLE > 180 and ANGLE < 270:
+		COOR[0]=(-b-sqrt((b^2)-(4*a*c)))/(2*a)
+	elif ANGLE > 270 and ANGLE < 0:
+		COOR[0]=(-b+sqrt((b^2)-(4*a*c)))/(2*a)
+	return(COOR)
 
-symbol = Image.new("RGB", (CANVAS,CANVAS), "white")
+def DrawSymbol(ROT, SHIFT, CANVAS, BUFFER):
+	Center=[.5,.5]
+	return()
+
+symbol = Image.new("RGB", (Canvas,Canvas), "white")
 drawsymbol = ImageDraw.Draw(symbol)
+
+DrawSymbol(Rot, Shift, Canvas, Buffer)
+
+symbol.show()
