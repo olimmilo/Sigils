df7363b91f0b50dd9dcb188f1b89c314ba3f7110
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-08 13:36:32 -0500

    Updated from Brython Server: 3/8/2018 1:36:32 PM

--- ggametester.py
+++ ggametester.py
@@ -1,15 +1,29 @@
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, LineAsset, PolygonAsset
+import math
 
-Color1=Color(0x000000, 1.0)
+Colors=[0xFE2712, 0xFC600A, 0xFB9902, 0xFCCC1A, 0xFEFE33, 0xB2D732, 0x66B032, 0x347C98, 0x0247FE, 0x4424D6, 0x8601AF, 0xC21460]
+Color1=[Color(q, 1.0) for q in Colors]
 Color2=Color(0x0000FF, 1.0)
 
-LC1=LineStyle(1, Color1)
-LC2=LineStyle(1, Color2)
+LC1=[LineStyle(1, r) for r in Color1]
 
+"""
 test=LineAsset(200, 300, LC1)
 Sprite(test, (900,300))
 test2=LineAsset(900, 300, LC2)
 Sprite(test2, (0,0))
+"""" 
+numrot=12
+i=0
+
+def radians(ANGLE):
+    angle=ANGLE*((2*math.pi)/360)
+    return(angle)
+
+
+while i < 12:
+    
+    i=i+1
 
 DrawSigil = App()
 DrawSigil.run()
