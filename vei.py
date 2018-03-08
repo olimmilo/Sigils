8c833253771edf6182bac4153271183ef174b6f7
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-08 15:52:13 -0500

    Updated from Brython Server: 3/8/2018 3:52:13 PM

--- vei.py
+++ vei.py
@@ -97,7 +97,7 @@ def DrawSymbol(ROT, SHIFT, CANVAS, BUFFER, COLOR1, COLOR2):
 		slope=degrees(math.atan((line[1][1]-line[0][1])/(line[1][0]-line[0][0])))
 		hypot=(line[1][0]-line[1][0])/(math.cos(slope))
 		TEMPline=LineAsset((line[1][0]-line[0][0]),(line[1][1]-line[0][1]), LC1)
-		"""
+		
 		if slope >= 0 and slope < 90:
 			Sprite(TEMPline, (line[0][0], line[0][1]))
 		elif slope >= 90 and slope < 180:
@@ -106,8 +106,8 @@ def DrawSymbol(ROT, SHIFT, CANVAS, BUFFER, COLOR1, COLOR2):
 			Sprite(TEMPline, (line[0][0]-(math.cos(slope)*hypot), line[0][1]-(math.sin(slope)*hypot)))
 		elif slope >= 270 and slope < 360:
 			Sprite(TEMPline, (line[0][0], line[0][1]-(math.sin(angle)*hypot)))
-		"""
-		Sprite(TEMPline, (line[0][0],line[0][1]))
+		
+		#Sprite(TEMPline, (line[0][0],line[0][1]))
 		NUMBER=LineAsset(100,250, LC1)
 		Sprite(NUMBER, (600+(50*n),500))
 		n=n+1
