8a774791c1f7712e768190b9576070c8a1cf0144
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-08 15:39:25 -0500

    Updated from Brython Server: 3/8/2018 3:39:25 PM

--- vei.py
+++ vei.py
@@ -100,8 +100,8 @@ def DrawSymbol(ROT, SHIFT, CANVAS, BUFFER, COLOR1, COLOR2):
 		hypot2=(line[1][1]-line[1][1])/(math.sin(slope))
 		if hypot == hypot2:
 		    print("yes")
-	    else:
-	        print("no")
+        else:
+            print("no")
 		if slope >= 0 and slope < 90:
 			Sprite(TEMPline, ((line[1][0]-line[0][0]), (line[1][1]-line[0][1])))
 		elif slope >= 90 and slope < 180:
