ee663d29b5db8925902588a0f8a5a85a6de80705
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-09 09:50:43 -0500

    Updated from Brython Server: 3/9/2018 9:50:41 AM

--- vei.py
+++ vei.py
@@ -99,7 +99,7 @@ def DrawSymbol(ROT, SHIFT, CANVAS, BUFFER, COLOR1, COLOR2):
 		hypot=(line[1][0]-line[0][0])/(math.cos(rslope))
 		hypot2=(line[1][1]-line[0][1])/(math.sin(rslope))
 		print(hypot-hypot2)
-		if slope == 0:
+		if slope == 0 or slope == 360:
 		    TEMPline=LineAsset((line[1][0]-line[0][0]),(line[1][1]-line[0][1]), LC1)
 		    Sprite(TEMPline, (line[0][0], line[0][1]))
 		elif slope == 90 or slope == 270:
