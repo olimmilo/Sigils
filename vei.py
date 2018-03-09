06e60f8e8d85386b3567b9092ccedfb5c8579801
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-03-09 11:00:18 -0500

    Updated from Brython Server: 3/9/2018 11:00:17 AM

--- vei.py
+++ vei.py
@@ -75,8 +75,8 @@ def DrawSymbol(ROT, SHIFT, CANVAS, BUFFER, COLOR1, COLOR2):
 	SCALE=CANVAS-BUFFER
 	PAD=BUFFER/2
 	LINELIST=[]
-	LC1=LineStyle(1, COLOR1)
-	LC2=LineStyle(1, COLOR2)
+	LC1=LineStyle(4, COLOR1)
+	LC2=LineStyle(4, COLOR2)
 	BG=Color(0xffffff, 0.0)
 	SCREEN=RectangleAsset(CANVAS, CANVAS, LC1, BG)
 	CIRCLE=CircleAsset((Radius*SCALE), LC1, BG)
@@ -143,8 +143,8 @@ def DrawSubSymbol(ROT, SHIFT, CANVAS, BUFFER, COLOR1, COLOR2):
 	SCALE=CANVAS-BUFFER
 	PAD=BUFFER/2
 	LINELIST=[]
-	LC1=LineStyle(2, COLOR1)
-	LC2=LineStyle(2, COLOR2)
+	LC1=LineStyle(4, COLOR1)
+	LC2=LineStyle(4, COLOR2)
 	BG=Color(0xffffff, 0.0)
 	
 	SEC=LineList(1, 0, SHIFT)
